e0931243b86eee6ca5a224e59c48354623c8beac
Kitti Sintuprasert <11dj@mcbk.local>
2022-01-20 12:20:21 +0700

    stream device1

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,8 +4,14 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:id_card_reader/common.dart';
 import 'package:id_card_reader/id_card_reader.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'dart:typed_data';
+
+import 'package:intl/intl.dart';
 
 void main() {
+  Intl.defaultLocale = 'th_TH';
+  initializeDateFormatting('th_TH', null);
   runApp(const MyApp());
 }
 
@@ -25,12 +31,14 @@ class _MyAppState extends State<MyApp> {
   var _open1;
   var _warm1;
   var _protocal1;
-  var _readCID;
+  var _data;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    // Intl.defaultLocale = 'th';
+    // initializeDateFormatting();
   }
 
   // Platform messages are asynchronous, so we initialize in an async method.
@@ -55,34 +63,34 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  _fn1() async {
-    print('Hello111');
-    List<UsbDevice> aa;
-    try {
-      aa = await IdCardReader.getDeviceList();
-      print('aax $aa');
-      setState(() {
-        _deviceList = aa;
-      });
-    } catch (e) {
-      setState(() {
-        _deviceList = 'ERR fn1 $e';
-      });
-    }
-  }
+  // _fn1() async {
+  //   print('Hello111');
+  //   List<UsbDevice> aa;
+  //   try {
+  //     aa = await IdCardReader.getDeviceList();
+  //     print('aax $aa');
+  //     setState(() {
+  //       _deviceList = aa;
+  //     });
+  //   } catch (e) {
+  //     setState(() {
+  //       _deviceList = 'ERR fn1 $e';
+  //     });
+  //   }
+  // }
 
-  _fn2() async {
-    try {
-      var aa = await IdCardReader.test1;
-      setState(() {
-        _t1 = aa;
-      });
-    } catch (e) {
-      setState(() {
-        _t1 = 'ERR fn2 $e';
-      });
-    }
-  }
+  // _fn2() async {
+  //   try {
+  //     var aa = await IdCardReader.test1;
+  //     setState(() {
+  //       _t1 = aa;
+  //     });
+  //   } catch (e) {
+  //     setState(() {
+  //       _t1 = 'ERR fn2 $e';
+  //     });
+  //   }
+  // }
 
   open1() async {
     try {
@@ -93,6 +101,7 @@ class _MyAppState extends State<MyApp> {
     } catch (e) {
       setState(() {
         _open1 = 'ERR open1 $e';
+        _data = null;
       });
     }
   }
@@ -106,33 +115,45 @@ class _MyAppState extends State<MyApp> {
     } catch (e) {
       setState(() {
         _warm1 = 'ERR warm1 $e';
+        _data = null;
       });
     }
   }
 
   protocal1() async {
     try {
-      var aa = await IdCardReader.protocal1;
+      var aa = await IdCardReader.protocol1;
       setState(() {
         _protocal1 = aa;
       });
     } catch (e) {
       setState(() {
         _protocal1 = 'ERR protocal1 $e';
+        _data = null;
       });
     }
   }
 
-  readCID() async {
+  readAll() async {
     try {
-      var aa = await IdCardReader.read1;
+      var aa = await IdCardReader.readAll;
       setState(() {
-        _readCID = aa;
+        _data = aa;
       });
     } catch (e) {
       setState(() {
-        _readCID = 'ERR readCID $e';
+        _data = 'ERR readAll $e';
       });
+    }
+  }
+
+  formattedDate(dt) {
+    try {
+      DateTime dateTime = DateTime.parse(dt);
+      String formattedDate = DateFormat.yMMMMd('th_TH').format(dateTime);
+      return formattedDate;
+    } catch (e) {
+      return e.toString();
     }
   }
 
@@ -141,45 +162,117 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('ACS Thai ID Card Reader'),
         ),
         body: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Center(
-                child: Text('Running on: $_platformVersion\n'),
+              Row(
+                children: [
+                  ElevatedButton(onPressed: open1, child: const Text('open')),
+                  ElevatedButton(onPressed: warm1, child: const Text('warm')),
+                  ElevatedButton(
+                      onPressed: protocal1, child: const Text('protocal')),
+                  ElevatedButton(onPressed: readAll, child: const Text('read')),
+                ],
               ),
-              StreamBuilder<int>(
-                stream: IdCardReader.getRandomNumberStream,
-                builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+              Text(_open1.toString()),
+              Text(_warm1.toString()),
+              Text(_protocal1.toString()),
+              if (_data != null) ...[
+                Center(
+                  child: Image.memory(
+                    Uint8List.fromList(_data.image),
+                  ),
+                ),
+                DisplayInfo(
+                    title: 'เลขบัตรประชาชน', value: '${_data.nationID}'),
+                DisplayInfo(
+                    title: 'ชื่อ-นามสกุล (ภาษาไทย)',
+                    value:
+                        '${_data.titleTH} ${_data.firstnameTH} ${_data.lastnameTH}'),
+                DisplayInfo(
+                    title: 'ชื่อ-นามสกุล (ภาษาอังกฤษ)',
+                    value:
+                        '${_data.titleEN} ${_data.firstnameEN} ${_data.lastnameEN}'),
+                DisplayInfo(
+                    title: 'เพศ',
+                    value:
+                        '(${_data.gender}) ${_data.gender == 1 ? 'ชาย' : 'หญิง'}'),
+                DisplayInfo(
+                    title: 'วันเดือนปีเกิด',
+                    value:
+                        '${_data.birthdate.toString()}\n${formattedDate(_data.birthdate)}'),
+                DisplayInfo(title: 'ที่อยู่', value: _data.address),
+                DisplayInfo(
+                    title: 'วันออกบัตร',
+                    value:
+                        '${_data.issueDate.toString()}\n${formattedDate(_data.issueDate)}'),
+                DisplayInfo(
+                    title: 'วันหมดอายุ',
+                    value:
+                        '${_data.expireDate.toString()}\n${formattedDate(_data.expireDate)}'),
+              ],
+              StreamBuilder(
+                stream: IdCardReader.getStream1,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.hasData) {
-                    return Text(
-                        "[EventChannel] Current Random Number: ${snapshot.data}");
+                    return Text("[EventChannel]: ${snapshot.data.toString()}");
                   } else {
-                    return const Text(
-                        "[EventChannel] Waiting for new random number...");
+                    return const Text("[EventChannel] Waiting....");
                   }
                 },
               ),
-              ElevatedButton(onPressed: _fn1, child: const Text('Device list')),
-              Text(_deviceList.toString()),
-              ElevatedButton(onPressed: _fn2, child: const Text('fn2 test1')),
-              Text(_t1.toString()),
-              ElevatedButton(onPressed: _fn2, child: const Text('fn2 test1')),
-              Text(_t1.toString()),
-              ElevatedButton(onPressed: open1, child: const Text('open1')),
-              Text(_open1.toString()),
-              ElevatedButton(onPressed: warm1, child: const Text('warm1')),
-              Text(_warm1.toString()),
-              ElevatedButton(
-                  onPressed: protocal1, child: const Text('protocal1')),
-              Text(_protocal1.toString()),
-              ElevatedButton(onPressed: readCID, child: const Text('readCID')),
-              Text(_readCID.toString()),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class DisplayInfo extends StatelessWidget {
+  const DisplayInfo({
+    Key? key,
+    required this.title,
+    required this.value,
+  }) : super(key: key);
+
+  final String title;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle sTitle =
+        const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+    TextStyle sVal = const TextStyle(fontSize: 28);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                '$title : ',
+                style: sTitle,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: Text(
+                  value,
+                  style: sVal,
+                ),
+              ),
+            ],
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+        ],
       ),
     );
   }
